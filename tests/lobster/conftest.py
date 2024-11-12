52c01bee99df7d53ac02ae468876e0de050082ca
Nathan Frey <ncfrey@users.noreply.github.com>
2024-11-12 16:23:34 -0500

    lint

--- tests/lobster/conftest.py
+++ tests/lobster/conftest.py
@@ -1,7 +1,6 @@
 import logging
 
 import pytest
-from lobster.data import NegLogDataModule
 
 
 @pytest.fixture(autouse=True)
@@ -10,14 +9,3 @@ def configure_logging():
     logger.setLevel(logging.DEBUG)
     for handler in logger.handlers:
         handler.setLevel(logging.DEBUG)
-
-
-@pytest.fixture(scope="session")
-def ppi_datamodule(tmp_path_factory):
-    root = tmp_path_factory.mktemp("neglog")
-    return NegLogDataModule(
-        root=root,
-        download=False,
-        lengths=[0.7, 0.2, 0.1],
-        truncation_seq_length=50,
-    )
