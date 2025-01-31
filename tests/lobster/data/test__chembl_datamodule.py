8ddf207f9bf4b8f888c40f204f6e1182ce66deb4
karinazad <karina.zadorozhny@gmail.com>
2025-01-31 14:52:55 -0500

    tests

---
+++ tests/lobster/data/test__chembl_datamodule.py
@@ -0,0 +1,41 @@
+import unittest.mock
+
+import pytest
+from beignet.datasets import ChEMBLDataset
+from lobster.data import ChEMBLLightningDataModule
+from pandas import DataFrame
+from torch import Size
+
+
+@pytest.fixture
+def dm(tmp_path):
+    with unittest.mock.patch("pandas.read_csv") as mock_read_csv:
+        mock_read_csv.return_value = DataFrame({"smiles": 10 * ["CCO"]})
+
+        datamodule = ChEMBLLightningDataModule(
+            root=tmp_path,
+            batch_size=8,
+            lengths=(0.8, 0.1, 0.1),
+            train=True,
+            download=False,
+        )
+        datamodule.prepare_data()
+        datamodule.setup()
+
+        return datamodule
+
+
+class TestM320MLightningDataModule:
+    def test_prepare_data(self, dm: ChEMBLLightningDataModule):
+        assert dm._dataset is not None
+        assert len(dm._dataset) == 10
+        assert isinstance(dm._dataset, ChEMBLDataset)
+
+    def test_setup(self, dm: ChEMBLLightningDataModule):
+        assert len(dm._train_dataset) == 8
+        assert len(dm._val_dataset) == 1
+        assert len(dm._test_dataset) == 1
+
+        batch = next(iter(dm.train_dataloader()))
+
+        assert batch["input_ids"].shape == Size([8, 1, 512])
---
+++ tests/lobster/data/test__m320m_datamodule.py
@@ -0,0 +1,41 @@
+import unittest.mock
+
+import pytest
+from lobster.data import M320MLightningDataModule
+from lobster.datasets import M320MDataset
+from pandas import DataFrame
+from torch import Size
+
+
+@pytest.fixture
+def dm(tmp_path):
+    with unittest.mock.patch("pandas.read_parquet") as mock_read_parquet:
+        mock_read_parquet.return_value = DataFrame({"smiles": 10 * ["CCO"], "Description": 10 * ["description"]})
+
+        datamodule = M320MLightningDataModule(
+            root=tmp_path,
+            batch_size=8,
+            lengths=(0.8, 0.1, 0.1),
+            train=True,
+            download=False,
+        )
+        datamodule.prepare_data()
+        datamodule.setup()
+
+        return datamodule
+
+
+class TestM320MLightningDataModule:
+    def test_prepare_data(self, dm: M320MLightningDataModule):
+        assert dm._dataset is not None
+        assert len(dm._dataset) == 10
+        assert isinstance(dm._dataset, M320MDataset)
+
+    def test_setup(self, dm: M320MLightningDataModule):
+        assert len(dm._train_dataset) == 8
+        assert len(dm._val_dataset) == 1
+        assert len(dm._test_dataset) == 1
+
+        batch = next(iter(dm.train_dataloader()))
+
+        assert batch["input_ids"].shape == Size([8, 1, 512])
