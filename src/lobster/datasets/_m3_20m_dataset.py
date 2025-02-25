f97894074dab3dd906896874c41b5ca7bf9ca1ce
karinazad <karina.zadorozhny@gmail.com>
2025-02-25 10:57:20 -0500

    iterable dataset

--- src/lobster/datasets/_m3_20m_dataset.py
+++ src/lobster/datasets/_m3_20m_dataset.py
@@ -1,10 +1,11 @@
 from pathlib import Path
-from typing import Callable, Tuple
+from typing import Callable, ClassVar, Sequence, Tuple
 
 import pandas
 import pooch
 from beignet.transforms import Transform
-from torch.utils.data import Dataset
+from datasets import load_dataset
+from torch.utils.data import Dataset, IterableDataset
 
 
 class M320MDataset(Dataset):
@@ -96,3 +97,76 @@ class M320MDataset(Dataset):
 
     def __len__(self) -> int:
         return len(self._x)
+
+
+class M320MIterableDataset(IterableDataset):
+    """Multi-Modal Molecular (M^3) Dataset with 20M compounds.
+    M3-20M is a large-scale multi-modal molecular dataset with over 20 million molecules,
+    integrating SMILES, molecular graphs, 3D structures, physicochemical properties,
+    and textual descriptions. Reference: https://arxiv.org/abs/2412.06847
+
+    This dataset version contains SMILES strings and text descriptions.
+
+    Example:
+
+    ```python
+    dataset = M320MDataset()
+    next(iter(dataset))
+
+    (
+        'CCCS(=O)c1ccc2[nH]c(=NC(=O)OC)[nH]c2c1',
+        'This molecule is a sulfoxide. It is functionally related to an albendazole...'
+    )
+    ```
+    """
+
+    SUPPORTED_SPLITS: ClassVar[list[str]] = ["train", "validation", "test"]
+
+    def __init__(
+        self,
+        root: str | Path | None = None,
+        *,
+        transform: Callable | Transform | None = None,
+        keys: Sequence[str] | None = None,
+        split: str = "train",
+    ):
+        """
+        Initialize the M320MDataset.
+
+        Parameters
+        ----------
+        root : str or Path or None, optional
+            Root directory where the dataset is stored or will be downloaded.
+        download : bool, optional
+            If True, download the dataset if not already present.
+        known_hash : str or None, optional
+            Known hash of the dataset file for verification.
+        transform : Callable or Transform or None, optional
+            Optional transform to be applied on a sample.
+        columns : list of str or None, optional
+            List of columns to be used from the dataset.
+        """
+        super().__init__()
+
+        if split not in self.SUPPORTED_SPLITS:
+            raise ValueError(f"Unsupported split: {split}")
+
+        self.split = split
+
+        self.dataset = load_dataset("karina-zadorozhny/M320M", split=split, cache_dir=root, streaming=True)
+
+        self.keys = ["smiles", "Description"] if keys is None else keys
+
+        self.transform = transform
+
+    def __iter__(self):
+        for sample in self.dataset:
+            x = tuple(sample[key] for key in self.keys)
+
+            if len(x) == 1:
+                x = x[0]
+
+            if self.transform is not None:
+                x = self.transform(x)
+
+            yield x
