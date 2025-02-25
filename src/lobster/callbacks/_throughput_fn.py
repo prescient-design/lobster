5c1aa371286da1c1d3ad12ac03d641c495a551f6
karinazad <karina.zadorozhny@gmail.com>
2025-02-25 20:15:52 +0000

    throughput

--- src/lobster/callbacks/_throughput_fn.py
+++ src/lobster/callbacks/_throughput_fn.py
@@ -7,3 +7,10 @@ def throughput_batch_size_fn(batch: tuple | dict[str, Any]) -> int:
         batch, _ = batch
 
     return batch["input_ids"].shape[0]
+
+
+def throughput_length_fn(batch: tuple | dict[str, Any]) -> int:
+    if isinstance(batch, tuple) and len(batch) == 2:
+        batch, _ = batch
+
+    return batch["input_ids"].squeeze(1).shape[1]
