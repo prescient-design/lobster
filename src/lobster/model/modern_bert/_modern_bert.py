7a3ba6a6d9c137f6b3792d124ea513179da9aa8e
karinazad <karina.zadorozhny@gmail.com>
2025-02-25 20:30:03 +0000

    token ids

--- src/lobster/model/modern_bert/_modern_bert.py
+++ src/lobster/model/modern_bert/_modern_bert.py
@@ -29,13 +29,17 @@ else:
 class FlexBERT(pl.LightningModule):
     def __init__(
         self,
+        vocab_size: int,
+        pad_token_id: int,
+        mask_token_id: int,
+        cls_token_id: int,
+        eos_token_id: int,
         lr: float = 1e-3,
         beta1: float = 0.9,
         beta2: float = 0.98,
         eps: float = 1e-12,
         num_training_steps: int = 10_000,
         num_warmup_steps: int = 1_000,
-        tokenizer: Union[str, PreTrainedTokenizer, PreTrainedTokenizerFast] = "amino_acid_tokenizer",
         mask_percentage: float = 0.25,
         max_length: int = 512,
         **model_kwargs,
@@ -50,46 +54,9 @@ class FlexBERT(pl.LightningModule):
         self._mask_percentage = mask_percentage
         self.max_length = max_length
 
-        # TODO zadorozk: currently only accepts one tokenizer at a time
-        # Extend to accept multiple tokenizers for each modality
-        if isinstance(tokenizer, str):
-            if tokenizer == "pmlm_tokenizer":
-                path = importlib.resources.files("lobster") / "assets" / "plm_tokenizer"
-                tokenizer = PmlmTokenizer.from_pretrained(path, do_lower_case=False)
-                tokenizer_transform_class = PmlmTokenizerTransform
-
-            elif tokenizer == "amino_acid_tokenizer":
-                tokenizer = AminoAcidTokenizerFast()
-                tokenizer_transform_class = TokenizerTransform
-
-            elif tokenizer == "nucleotide_tokenizer":
-                tokenizer = NucleotideTokenizerFast()
-                tokenizer_transform_class = TokenizerTransform
-
-            elif tokenizer == "smiles_tokenizer":
-                tokenizer = SmilesTokenizerFast()
-                tokenizer_transform_class = TokenizerTransform
-            else:
-                raise NotImplementedError(f"Tokenizer `{tokenizer}` not supported")
-        else:
-            if not isinstance(tokenizer, (PreTrainedTokenizer, PreTrainedTokenizerFast)):
-                raise ValueError("Custom `tokenizer` must be an instance of `PreTrainedTokenizer` or `PreTrainedTokenizerFast`")
-
-            tokenizer = tokenizer
-            tokenizer_transform_class = TokenizerTransform
-
-        self.tokenizer = tokenizer
-
-        self.tokenize_transform = tokenizer_transform_class(
-            tokenizer,
-            max_length=max_length,
-            padding="max_length",
-            truncation=True
-            )
-
         self.config = FlexBertConfig(
-            vocab_size=self.tokenizer.vocab_size,
-            pad_token_id=self.tokenizer.pad_token_id,
+            vocab_size=vocab_size,
+            pad_token_id=pad_token_id,
             **model_kwargs,
         )
         self.model = FlexBertModel(self.config)
@@ -102,6 +69,12 @@ class FlexBERT(pl.LightningModule):
         assert _FLASH_ATTN_AVAILABLE, "flash_attn not available. This dependency is part of the flash extra"
         self.loss_fn = CrossEntropyLoss()
         self.save_hyperparameters(logger=False)
+
+        self.vocab_size = vocab_size
+        self.pad_token_id = pad_token_id
+        self.mask_token_id = mask_token_id
+        self.cls_token_id = cls_token_id
+        self.eos_token_id = eos_token_id
 
     def training_step(self, batch, batch_idx):
         loss = self._compute_loss(batch)
@@ -160,22 +133,6 @@ class FlexBERT(pl.LightningModule):
         
         return hidden_states
 
-    def sequences_to_latents(self, sequences: list[str]) -> list[torch.Tensor]:
-        transformed_sequences = self.tokenize_transform(sequences)
-        input_ids = torch.concat([batch["input_ids"].squeeze(0) for batch in transformed_sequences]).to(self.device)
-        attention_mask = torch.concat([batch["attention_mask"].squeeze(0) for batch in transformed_sequences]).to(
-            self.device
-        )
-        seqlens = [batch["input_ids"].size(1) for batch in transformed_sequences]
-        cu_seqlens = torch.tensor([sum(seqlens[:i]) for i in range(len(seqlens) + 1)], dtype=torch.int32).to(
-            self.device
-        )
-
-        with torch.inference_mode():
-            hidden_states = self.model(input_ids, attention_mask=attention_mask, cu_seqlens=cu_seqlens, max_seqlen=self.max_length)
-
-        return [hidden_states[cu_seqlens[i] : cu_seqlens[i + 1]] for i in range(len(cu_seqlens) - 1)]
-
     def _compute_loss(self, batch):
 
         if isinstance(batch, tuple) and len(batch) == 2:
@@ -189,11 +146,14 @@ class FlexBERT(pl.LightningModule):
         labels = tokens.clone()
 
         masked_tokens = self._mask_inputs(tokens)
-        labels[masked_tokens != self.tokenizer.mask_token_id] = -100  # Ignore loss on unmasked tokens
+        labels[masked_tokens != self.mask_token_id] = -100  # Ignore loss on unmasked tokens
 
         # Cumulative sequence lengths.
         # TODO: Probably we can/should throw away trailing <pad> tokens.
         cu_seqlens = torch.tensor([0] + [(i + 1) * length for i in range(B)], dtype=torch.int32).cuda()
+
+        assert masked_tokens.max() < self.config.vocab_size, f"Token ID {masked_tokens.max()} is out of vocabulary range {self.config.vocab_size}"
+
         hidden_states = self.model(
             masked_tokens,
             attention_mask=attention_mask,
@@ -203,7 +163,7 @@ class FlexBERT(pl.LightningModule):
 
         logits = self.decoder(hidden_states)
 
-        return self.loss_fn(logits.view(-1, self.tokenizer.vocab_size), labels.view(-1))
+        return self.loss_fn(logits.view(-1, self.vocab_size), labels.view(-1))
 
     def _mask_inputs(self, train_inputs: torch.Tensor):
         # create random array of floats with equal dimensions to input_ids tensor
@@ -212,13 +172,13 @@ class FlexBERT(pl.LightningModule):
         # create mask array
         # TODO: update this for special cls tokens that might be introduced with the new tokenizer
         mask_arr = (
-            (rand < self._mask_percentage)
-            * (train_inputs != self.tokenizer.cls_token_id)
-            * (train_inputs != self.tokenizer.pad_token_id)
-            * (train_inputs != self.tokenizer.eos_token_id)
+            (rand < self._mask_percentage)``
+            * (train_inputs != self.cls_token_id)
+            * (train_inputs != self.pad_token_id)
+            * (train_inputs != self.eos_token_id)
         )  # don't mask cls, pad, eos
 
         masked_inputs = train_inputs.clone()
-        masked_inputs[mask_arr] = self.tokenizer.mask_token_id
+        masked_inputs[mask_arr] = self.mask_token_id
 
         return masked_inputs
