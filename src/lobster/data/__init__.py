e11df6dbb435c47c641d8c03a300e1eef851549c
karinazad <karina.zadorozhny@gmail.com>
2025-02-26 09:41:08 -0500

    imports

--- src/lobster/data/__init__.py
+++ src/lobster/data/__init__.py
@@ -17,9 +17,7 @@ from ._minhasher import LobsterMinHasher
 from ._mmseqs import MMSeqsRunner
 from ._structure_datamodule import PDBDataModule
 from ._ume_datamodule import UmeLightningDataModule
-from ._ume_haha_datamodule import UmeHahaLightningDataModule
 from ._utils import load_pickle
-from ._weighted_concat_sampler import WeightedConcatSampler
 
 __all__ = [
     "CalmLightningDataModule",
@@ -39,7 +37,5 @@ __all__ = [
     "MMSeqsRunner",
     "PDBDataModule",
     "load_pickle",
-    "WeightedConcatSampler",
     "UmeLightningDataModule",
-    "UmeHahaLightningDataModule",
 ]
