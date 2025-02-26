b1668cdbec3881e6829a59654362ee368382cc14
karinazad <karina.zadorozhny@gmail.com>
2025-02-26 12:18:17 -0500

    ruff constants

--- src/lobster/constants/__init__.py
+++ src/lobster/constants/__init__.py
@@ -1,6 +1,5 @@
 from ._modality import Modality
-from ._weighted_concat_sampler_chunk_size import WEIGHTED_CONCAT_SAMPLER_CHUNK_SIZE
 from ._moleculeace_tasks import MOLECULEACE_TASKS
+from ._weighted_concat_sampler_chunk_size import WEIGHTED_CONCAT_SAMPLER_CHUNK_SIZE
 
 __all__ = ["Modality", "WEIGHTED_CONCAT_SAMPLER_CHUNK_SIZE", "MOLECULEACE_TASKS"]
-
