b2cce970b03630dca21b2f5579ef85571c7f447d
Sidney Lisanza <lisanzas@gene.com>
2025-03-06 16:39:59 -0500

    lg tokenizer assets

--- src/lobster/tokenization/__init__.py
+++ src/lobster/tokenization/__init__.py
@@ -5,6 +5,7 @@ from ._latent_generator_3d_coord_tokenizer import LatentGenerator3DCoordTokenize
 from ._mgm_tokenizer import MgmTokenizer
 from ._mgm_tokenizer_transform import MgmTokenizerTransform
 from ._nucleotide_tokenizer import NucleotideTokenizerFast
+from ._latent_generator_tokens import LatentGeneratorTokenizerFast
 from ._pmlm_custom_concept_tokenizer_transform import (
     CUSTOM_TOKENIZER,
     UnirefDescriptorTransform,
