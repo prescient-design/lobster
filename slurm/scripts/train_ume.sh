ac0097b6b860cfe874267eb5f08077901d22d4c5
Karina Zadorozhny <karina.zadorozhny@gmail.com>
2025-04-16 12:41:27 -0400

    update flash attention wheels (#61)
    * lock
    
    * torch 2.5
    
    * torch 2.5
    
    * part
    
    * .env

--- slurm/scripts/train_ume.sh
+++ slurm/scripts/train_ume.sh
@@ -2,19 +2,18 @@
 
 #SBATCH --partition gpu2
 #SBATCH --nodes 1
-#SBATCH --ntasks-per-node 2
-#SBATCH --gpus-per-node 2
+#SBATCH --ntasks-per-node 1
+#SBATCH --gpus-per-node 1
 #SBATCH --cpus-per-task 8
 #SBATCH -o slurm/logs/%J.out
-#SBATCH -t 24:00:00
+#SBATCH -t 00-24:00:00
+
 # srun hostname
 
 nvidia-smi
 
 source .venv/bin/activate
 echo "SLURM_JOB_ID = ${SLURM_JOB_ID}"
-
-wandb login --host $WANDB_BASE_URL
 
 export WANDB_INSECURE_DISABLE_SSL=true
 export HYDRA_FULL_ERROR=1
@@ -29,7 +28,6 @@ export TOKENIZERS_PARALLELISM=true
 umask g+w 
 
 srun -u --cpus-per-task 8 --cpu-bind=cores,verbose \
-lobster_train experiment=train_ume ++data.datasets="['M320M', 'Calm', 'AMPLIFY', 'ZINC']"\
-    logger.entity="$(whoami)" 
+lobster_train experiment=train_ume
 
 
