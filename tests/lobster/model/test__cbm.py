98b0d3274237fb415f2b60d5a83d262c2a4ee844
Joseph Kleinhenz <kleinhej@gene.com>
2025-07-01 09:59:01 -0700

    add checkpoint tests

--- tests/lobster/model/test__cbm.py
+++ tests/lobster/model/test__cbm.py
@@ -3,6 +3,7 @@ import shutil
 import tempfile
 
 import pytest
+import torch
 from torch import Size, Tensor
 
 from lobster.model import LobsterCBMPMLM
@@ -42,3 +43,31 @@ class TestLobsterCBMPMLM:
         )
 
         assert model.config.hidden_size == 408
+
+
+def test_cbmlm_checkpoint(tmp_path):
+    print(f"{tmp_path=}")
+    model = LobsterCBMPMLM("MLM_mini")
+
+    for k, v in model.named_parameters():
+        torch.nn.init.normal_(v)
+
+    model.save_pretrained(tmp_path / "checkpoint")
+
+    model2 = LobsterCBMPMLM(str(tmp_path / "checkpoint"))
+
+    for (k1, v1), (k2, v2) in zip(model.named_parameters(), model2.named_parameters()):
+        assert k1 == k2
+        assert torch.equal(v1, v2)
+        assert not torch.equal(v2, torch.zeros_like(v2)), f"{k1=}, {k2=}"
+
+    assert torch.equal(model.model.lm_head.bias, model2.model.lm_head.bias)
+
+    input = torch.randn(2, 56)
+    output = model.model.lm_head.decoder(input)
+    output2 = model2.model.lm_head.decoder(input)
+
+    diff = output - output2
+    print(f"{diff.abs().max()=}")
+
+    torch.testing.assert_close(output, output2)
