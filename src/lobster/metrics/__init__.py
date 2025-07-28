012bb0f689c0da44416f1d47430e605753ba2655
Karina Zadorozhny <karina.zadorozhny@gmail.com>
2025-07-28 10:28:57 +0200

    remove metrics from this MR

--- src/lobster/metrics/__init__.py
+++ src/lobster/metrics/__init__.py
@@ -1,5 +1,4 @@
 from ._binary_classification import summarize_binary_classification_metrics
-from ._contrastive_retrieval_accuracy import ContrastiveRetrievalAccuracy
 from ._perturbation_score import PerturbationScore
 from ._random_neighbor_score import RandomNeighborScore
 
@@ -7,5 +6,4 @@ __all__ = [
     "summarize_binary_classification_metrics",
     "RandomNeighborScore",
     "PerturbationScore",
-    "ContrastiveRetrievalAccuracy",
 ]
