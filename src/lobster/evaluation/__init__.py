1f0e3954980ffbd11d10286610d02710f9048142
Taylor Joren <joren.taylor@gene.com>
2025-08-11 22:02:38 +0000

    ws

--- src/lobster/evaluation/__init__.py
+++ src/lobster/evaluation/__init__.py
@@ -3,9 +3,4 @@ from .dgeb_adapter import UMEAdapterDGEB
 from .esm_dgeb_adapter import ESMAdapterDGEB
 from .dgeb_runner import run_evaluation, generate_report
 
-__all__ = [
-    "evaluate_model_with_callbacks", 
-    "UMEAdapterDGEB", 
-    "ESMAdapterDGEB", 
-    "run_evaluation", 
-    "generate_report"]
+__all__ = ["evaluate_model_with_callbacks", "UMEAdapterDGEB", "ESMAdapterDGEB", "run_evaluation", "generate_report"]
