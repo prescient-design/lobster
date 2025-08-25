93817ef7b6bbdb202c4e91227e85617d471e3ac4
Karina Zadorozhny <karina.zadorozhny@gmail.com>
2025-08-25 15:40:08 -0400

    logging level

--- src/lobster/cmdline/train.py
+++ src/lobster/cmdline/train.py
@@ -7,11 +7,11 @@ from lightning.pytorch.utilities import rank_zero_only
 from omegaconf import DictConfig, OmegaConf
 
 import wandb
-from lobster.cmdline._utils import instantiate_callbacks
+from lobster.cmdline._utils import instantiate_callbacks, configure_logging
 
 dotenv.load_dotenv(".env")
 
-logging.basicConfig(level=logging.INFO)
+configure_logging(level=logging.INFO)
 
 
 @hydra.main(version_base=None, config_path="../hydra_config", config_name="train")
