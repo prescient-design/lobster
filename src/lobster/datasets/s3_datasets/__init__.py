bce9a32de806173bbb1774cf5ed2899b312a35ed
Karina Zadorozhny <karina.zadorozhny@gmail.com>
2025-09-03 13:05:28 -0400

    streaming dataset name

--- src/lobster/datasets/s3_datasets/__init__.py
+++ src/lobster/datasets/s3_datasets/__init__.py
@@ -2,26 +2,16 @@ from .base import UMEStreamingDataset
 from .amplify import AMPLIFY
 from .atomica import Atomica
 from .calm import Calm
-from .dev.fsmol import FSMol
-from .dev.geom_lg import GeomLG
 from .m320m import M320M
-from .dev.opengenome2 import OpenGenome2
-from .pdbbind_lg import PDBBindLG
 from .peptide_atlas import PeptideAtlas
-from .pinder_lg import PinderLG
 from .zinc import ZINC
 
 __all__ = [
     "AMPLIFY",
     "Calm",
     "M320M",
-    "OpenGenome2",
     "PeptideAtlas",
     "ZINC",
     "UMEStreamingDataset",
-    "PinderLG",
     "Atomica",
-    "GeomLG",
-    "PDBBindLG",
-    "FSMol",
 ]
