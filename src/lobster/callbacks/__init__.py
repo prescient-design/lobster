cb7dfd3172777eaec00b291a23598cdde6a225e4
karinazad <karina.zadorozhny@gmail.com>
2025-09-23 17:29:27 +0000

    callbacks

--- src/lobster/callbacks/__init__.py
+++ src/lobster/callbacks/__init__.py
@@ -9,7 +9,6 @@ from ._tokens_per_second_callback import TokensPerSecondCallback, default_batch_
 from ._umap_visualization_callback import UmapVisualizationCallback
 from ._ume_grpo_logging_callback import UmeGrpoLoggingCallback
 from ._auxiliary_task_loss_weight_scheduler import AuxiliaryTaskWeightScheduler, MultiTaskWeightScheduler
-from ._simplified_peer_evaluation_callback import SimplifiedPEEREvaluationCallback
 
 __all__ = [
     "MoleculeACELinearProbeCallback",
