46e11ed550588e955cf4f1f2bc598a68e634e6db
Lisanza <lisanzas@b200-st-b200-2-9.b200.pcluster>
2025-09-26 15:29:06 +0000

    fix errors

--- src/lobster/model/gen_ume/__init__.py
+++ src/lobster/model/gen_ume/__init__.py
@@ -1,4 +1,8 @@
-from ._gen_ume_sequence_structure_encoder import AuxiliaryRegressionTaskHead, AuxiliaryTask, UMESequenceStructureEncoderModule
+from ._gen_ume_sequence_structure_encoder import (
+    AuxiliaryRegressionTaskHead,
+    AuxiliaryTask,
+    UMESequenceStructureEncoderModule,
+)
 from ._gen_ume_sequence_structure_encoder_lightning_module import UMESequenceStructureEncoderLightningModule
 
 __all__ = [
