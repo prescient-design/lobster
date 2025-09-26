717c69679f48c57faf01eb5bd0b9434546f544cf
Lisanza <lisanzas@b200-st-b200-2-9.b200.pcluster>
2025-09-26 16:32:01 +0000

    ruff

--- src/lobster/data/__init__.py
+++ src/lobster/data/__init__.py
@@ -16,7 +16,8 @@ from ._m3_20m_datamodule import M320MLightningDataModule
 from ._minhasher import LobsterMinHasher
 from ._mmseqs import MMSeqsRunner
 from ._structure_datamodule import PDBDataModule
-#from ._coord_structure_datamodule import StructureLightningDataModule
+
+# from ._coord_structure_datamodule import StructureLightningDataModule
 from ._ume_datamodule import UMELightningDataModule
 from ._utils import download_from_s3, get_s3_bucket_and_key, load_pickle, upload_to_s3
 
@@ -37,7 +38,7 @@ __all__ = [
     "LobsterMinHasher",
     "MMSeqsRunner",
     "PDBDataModule",
-    #"StructureLightningDataModule",
+    # "StructureLightningDataModule",
     "load_pickle",
     "UMELightningDataModule",
     "upload_to_s3",
