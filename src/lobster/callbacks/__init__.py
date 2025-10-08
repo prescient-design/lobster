5def174e3db87153373a19e7430777e38b47e0ec
karinazad <karina.zadorozhny@gmail.com>
2025-10-08 12:44:39 +0000

    callback updates

--- src/lobster/callbacks/__init__.py
+++ src/lobster/callbacks/__init__.py
@@ -4,7 +4,7 @@ from ._dataloader_checkpoint_callback import DataLoaderCheckpointCallback
 from ._dgeb_evaluation_callback import DGEBEvaluationCallback
 from ._moleculeace_sklearn_probe_callback import MoleculeACESklearnProbeCallback
 
-# from ._peer_evaluation_callback import PEEREvaluationCallback
+from ._peer_sklearn_probe_callback import PEERSklearnProbeCallback
 from ._perturbation_score_callback import PerturbationScoreCallback
 from ._sklearn_probe_callback import SklearnProbeCallback, SklearnProbeTaskConfig
 from ._tokens_per_second_callback import TokensPerSecondCallback, default_batch_length_fn, default_batch_size_fn
@@ -20,7 +20,7 @@ __all__ = [
     "SklearnProbeCallback",
     "MoleculeACESklearnProbeCallback",
     "CalmSklearnProbeCallback",
-    "PEEREvaluationCallback",
+    "PEERSklearnProbeCallback",
     "PerturbationScoreCallback",
     "TokensPerSecondCallback",
     "default_batch_length_fn",
