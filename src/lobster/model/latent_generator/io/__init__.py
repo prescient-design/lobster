87c6a7053b0bdda4b20e0be5abee984cff7d96e6
Robert Calef <robert.calef@gmail.com>
2025-10-16 15:16:28 -0700

    Add function for parsing text with LG token strings to token tensors

--- src/lobster/model/latent_generator/io/__init__.py
+++ src/lobster/model/latent_generator/io/__init__.py
@@ -1,2 +1,8 @@
 from ._load_pdb import load_ligand, load_pdb
 from ._write_pdb import writepdb, writepdb_ligand_complex
+from ._token_from_text import (
+    parse_tokens_from_text,
+    LG_START_TOK,
+    LG_END_TOK,
+    LG_TOK_TEMPLATE,
+)
